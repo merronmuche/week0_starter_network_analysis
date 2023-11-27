af6bb324f8b01b71cc5a70fe6bde6229c7f9b5b3
Meron Muche <merronmuche.com>
2023-11-27 22:47:43 +0300

    move slack_parser function from parse_slack_data.ipynb to loader.py

--- src/loader.py
+++ src/loader.py
@@ -8,6 +8,21 @@ from datetime import datetime
 from pick import pick
 from time import sleep
 
+import os, sys
+import re
+import json
+import glob
+import datetime
+from collections import Counter
+
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+from nltk.corpus import stopwords
+from wordcloud import WordCloud
+
+
 
 
 # Create wrapper classes for using slack_sdk in place of slacker
@@ -71,7 +86,79 @@ class SlackDataLoader:
         for user in self.users:
             userNamesById[user['id']] = user['name']
             userIdsByName[user['name']] = user['id']
-        return userNamesById, userIdsByName        
+        return userNamesById, userIdsByName    
+
+    # combine all json file in all-weeks8-9
+    def slack_parser(self, path_channel):
+        """ parse slack data to extract useful informations from the json file
+            step of execution
+            1. Import the required modules
+            2. read all json file from the provided path
+            3. combine all json files in the provided path
+            4. extract all required informations from the slack data
+            5. convert to dataframe and merge all
+            6. reset the index and return dataframe
+        """
+
+        # specify path to get json files
+        combined = []
+        files = glob.glob(f'{path_channel}*.json')
+        print(files)
+        for json_file in files:
+            print(json_file)
+            with open(json_file, 'r', encoding="utf8") as slack_data:
+                combined.append(json.loads(slack_data.read()))
+        print('//////////////////////////////')
+        print(combined)
+        # loop through all json files and extract required informations
+        dflist = []
+        for slack_data in combined:
+
+            msg_type, msg_content, sender_id, time_msg, msg_dist, time_thread_st, reply_users, \
+            reply_count, reply_users_count, tm_thread_end = [],[],[],[],[],[],[],[],[],[]
+
+            for row in slack_data:
+                if 'bot_id' in row.keys():
+                    continue
+                else:
+                    msg_type.append(row['type'])
+                    msg_content.append(row['text'])
+                    if 'user_profile' in row.keys(): sender_id.append(row['user_profile']['real_name'])
+                    else: sender_id.append('Not provided')
+                    time_msg.append(row['ts'])
+                    if 'blocks' in row.keys() and len(row['blocks'][0]['elements'][0]['elements']) != 0 :
+                        msg_dist.append(row['blocks'][0]['elements'][0]['elements'][0]['type'])
+                    else: msg_dist.append('reshared')
+                    if 'thread_ts' in row.keys():
+                        time_thread_st.append(row['thread_ts'])
+                    else:
+                        time_thread_st.append(0)
+                    if 'reply_users' in row.keys(): reply_users.append(",".join(row['reply_users'])) 
+                    else:    reply_users.append(0)
+                    if 'reply_count' in row.keys():
+                        reply_count.append(row['reply_count'])
+                        reply_users_count.append(row['reply_users_count'])
+                        tm_thread_end.append(row['latest_reply'])
+                    else:
+                        reply_count.append(0)
+                        reply_users_count.append(0)
+                        tm_thread_end.append(0)
+            data = zip(msg_type, msg_content, sender_id, time_msg, msg_dist, time_thread_st,
+            reply_count, reply_users_count, reply_users, tm_thread_end)
+            columns = ['msg_type', 'msg_content', 'sender_name', 'msg_sent_time', 'msg_dist_type',
+            'time_thread_start', 'reply_count', 'reply_users_count', 'reply_users', 'tm_thread_end']
+
+            df = pd.DataFrame(data=data, columns=columns)
+            df = df[df['sender_name'] != 'Not provided']
+            dflist.append(df)
+
+        dfall = pd.concat(dflist, ignore_index=True)
+        dfall['channel'] = path_channel.split('/')[-1].split('.')[0]        
+        dfall = dfall.reset_index(drop=True)
+        
+        return dfall
+
+    
 
 
 
@@ -82,3 +169,7 @@ if __name__ == "__main__":
     
     parser.add_argument('--zip', help="Name of a zip file to import")
     args = parser.parse_args()
+
+    slack_loader = SlackDataLoader('D:/tenacademy/codes/week0_starter_network_analysis/data/anonymized/')
+    path_channel = 'D:/tenacademy/codes/week0_starter_network_analysis/data/anonymized/all-week1/'
+    slack_loader.slack_parser(path_channel)
